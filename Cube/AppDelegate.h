2106e094c663bf8954c83cc2aa47a02109aab17e
mac <admin@xanadutec.local>
2016-07-26 11:41:26 +0530

    Initial Commit

---
+++ Cube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cube
+//
+//  Created by mac on 26/07/16.
+//  Copyright © 2016 Xanadutec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
